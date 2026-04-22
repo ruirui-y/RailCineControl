b37931996d56bc9a457c77308139f449ff06df93
13083361602 <3265868153@qq.com>
2026-04-22 18:48:08 +0800

    重构AccountWidget窗口，整合QSS

--- RailCineControl/AccountWidget.h
+++ RailCineControl/AccountWidget.h
@@ -1,29 +1,28 @@
 #ifndef ACCOUNT_WIDGET_H
 #define ACCOUNT_WIDGET_H
 
-#include <QWidget>
+#include "CinemaDialogBase.h"                                                   
 
 class QLabel;
+class QPushButton;
 
-class AccountWidget : public QWidget {
+class AccountWidget : public CinemaDialogBase
+{
     Q_OBJECT
+
 public:
     explicit AccountWidget(QWidget* parent = nullptr);
     void setUserName(const QString& name);
-
-protected:
-    void paintEvent(QPaintEvent*) override;
 
 private:
     void BuildUI();
 
 private slots:
-    void SlotLoginOut();                                                                                // 登出
+    void SlotLoginOut();                                                        // 登出触发槽
 
 private:
-    QLabel* m_name = nullptr;
-    class ImageBgButton* switchBtn = nullptr;
+    QLabel* m_name = nullptr;                                                   // 账号名称动态标签
+    QPushButton* switchBtn = nullptr;                                           // 切换/登出核心按钮
 };
-
 
 #endif // !ACCOUNT_WIDGET_H
