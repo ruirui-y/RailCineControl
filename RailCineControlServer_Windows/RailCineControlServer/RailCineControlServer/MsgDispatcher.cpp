328fbc4bba55a4f1868ae88c48755d4048bcb237
13083361602 <3265868153@qq.com>
2026-05-23 18:15:21 +0800

    删除ClientSession的事件分发器，独立出单例，避免每个会话独占一个事件分发器

---
+++ RailCineControlServer_Windows/RailCineControlServer/RailCineControlServer/MsgDispatcher.cpp
@@ -0,0 +1,22 @@
+﻿#include "MsgDispatcher.h"
+
+void MsgDispatcher::RegisterRoute(ServerApi::MsgId msgId, MsgHandler handler)
+{
+    if (m_router.contains(msgId)) {
+        qDebug() << u8"⚠️ [MsgDispatcher] 警告：重复注册消息路由 ID:" << msgId;
+        return;
+    }
+    m_router.insert(msgId, handler);
+}
+
+void MsgDispatcher::Dispatch(std::shared_ptr<ClientSession> session, ServerApi::MsgId msgId, const ServerApi::PacketHeader& header, const QByteArray& body)
+{
+    auto it = m_router.find(msgId);
+    if (it != m_router.end()) {
+        // 找到了对应的处理函数，直接执行！
+        it.value()(session, header, body);
+    }
+    else {
+        qDebug() << u8"❌ [MsgDispatcher] 未知消息类型，无法路由丢弃，MsgId:" << msgId;
+    }
+}
---
+++ RailCineControlServer_Windows/RailCineControlServer/RailCineControlServer/MsgDispatcher.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <QMap>
+#include <QObject>
+#include <functional>
+#include <memory>
+#include <QDebug>
+#include "singletion.h"                                                 
+#include "server_msg.pb.h"                                              
+
+class ClientSession;                                                    
+
+// 定义标准的消息处理器签名 (形参：会话智能指针, 包头, 包体)
+using MsgHandler = std::function<void(std::shared_ptr<ClientSession> session, const ServerApi::PacketHeader&, const QByteArray&)>;
+
+class MsgDispatcher : public QObject, public Singleton<MsgDispatcher>
+{
+    Q_OBJECT
+
+public:
+    friend class Singleton<MsgDispatcher>;
+
+public:
+    ~MsgDispatcher() = default;
+
+public:
+    // 注册路由 (建议在 Server 启动前的单线程环境中统一注册)
+    void RegisterRoute(ServerApi::MsgId msgId, MsgHandler handler);
+
+    // 分发消息 (供 ClientSession 收到数据后调用)
+    void Dispatch(std::shared_ptr<ClientSession> session, ServerApi::MsgId msgId, const ServerApi::PacketHeader& header, const QByteArray& body);
+
+private:
+    MsgDispatcher(QObject* parent = 0) {}
+    
+    // 核心路由表：MsgId -> 业务处理函数的映射
+    QMap<ServerApi::MsgId, MsgHandler> m_router;
+};
